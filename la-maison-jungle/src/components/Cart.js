7a93d29913476a3b8d41cfcc78e31004e116fbe9
Lucile16 <lulubeucher@gmail.com>
2023-04-23 16:54:09 +0200

    cart added

---
+++ la-maison-jungle/src/components/Cart.js
@@ -0,0 +1,16 @@
+function Cart() {
+    const monsteraPrice = 8
+    const ivyPrice = 10
+    const flowersPrice = 15
+    return (<div>
+        <h2>Panier</h2>
+        <ul>
+            <li>Monstera : {monsteraPrice} €</li>
+            <li>Lierre : {ivyPrice} €</li>
+            <li>Bouquet de fleurs : {flowersPrice} €</li>
+        </ul>
+        Total : {monsteraPrice + ivyPrice + flowersPrice} €
+    </div>)
+}
+
+export default Cart
